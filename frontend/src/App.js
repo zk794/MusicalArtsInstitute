89065a62f9fc4adecc4127008f45b4d8378ca460
nzliu6 <nzliu@uchicago.edu>
2021-02-07 13:21:57 -0600

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Admin from "./adminPage";
+import Student from "./studentPage";
+import Parent from "./parentPage";
+import Staff from "./staffPage";
 class App extends Component {
   render() {
     return (
@@ -11,6 +14,9 @@ class App extends Component {
         <Navbar />
         <Switch>
           <Route exact path="/admin" component={Admin} />
+          <Route exact path="/student" component={Student} />
+          <Route exact path="/parent" component={Parent} />
+          <Route exact path="/staff" component={Staff} />
           <Route exact strict path="/" component={Landing} />
         </Switch>
         {/* <Route exact path="/register" component={Register} />
