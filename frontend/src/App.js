f3d7b045cc9e2a295fe71133c546a24ce15d5888
nzliu6 <nzliu@uchicago.edu>
2021-02-07 15:01:06 -0600

    added calendar stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <Switch>
           <Route exact path="/admin" component={Admin} />
           <Route exact path="/student" component={Student} />
