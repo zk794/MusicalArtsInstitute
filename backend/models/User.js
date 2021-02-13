b170282dddcc12c856459a9b1c9a5eb7bb2d1837
nzliu6 <nzliu@uchicago.edu>
2021-02-12 21:27:47 -0600

    finished backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 // Create Schema
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
+  userRole: { type: String, required: true },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
+
 module.exports = User = mongoose.model("users", UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,12 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 
-const users = require("./routes/users");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const users = require("./routes/api/users");
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -50,10 +15,12 @@ app.use(
 );
 app.use(bodyParser.json());
 
-// setting up database
 // DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
 const URI =
-  "mongodb+srv://firstname:password@cluster0.qjuew.mongodb.net/MusicalArtsInstitute?retryWrites=true&w=majority";
+  "mongodb+srv://nick:musical@cluster0.qjuew.mongodb.net/MusicalArtsInstitute?retryWrites=true&w=majority";
 // Connect to MongoDB
 mongoose
   .connect(URI, {
@@ -65,12 +32,13 @@ mongoose
 
 // Passport middleware
 app.use(passport.initialize());
+
 // Passport config
-require("./routes/passport")(passport);
+require("./config/passport")(passport);
+
 // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
-module.exports = app;
+const port = process.env.PORT || 5000;
 
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
